dea0de9c2d779496ab51a197beb1effd0cbb0def
Will Hu <chopin.champagne@gmail.com>
2017-11-07 21:14:16 +0800

    first commit

---
+++ src/main/java/com/howtographql/hackernews/GraphQLEndpoint.java
@@ -0,0 +1,55 @@
+package com.howtographql.hackernews;
+
+import com.coxautodev.graphql.tools.SchemaParser;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import graphql.schema.GraphQLSchema;
+import graphql.servlet.GraphQLContext;
+import graphql.servlet.SimpleGraphQLServlet;
+
+import java.util.Optional;
+
+
+@WebServlet(urlPatterns = "/graphql")
+public class GraphQLEndpoint extends SimpleGraphQLServlet {
+
+    private static final LinkRepository linkRepository;
+    private static final UserRepository userRepository;
+
+    static {
+        MongoDatabase mongo = new MongoClient().getDatabase("hackernews");
+        linkRepository = new LinkRepository(mongo.getCollection("links"));
+        userRepository = new UserRepository(mongo.getCollection("users"));
+
+    }
+
+    public GraphQLEndpoint() {
+        super(buildSchema());
+    }
+
+    @Override
+    protected GraphQLContext createContext(Optional<HttpServletRequest> request, Optional<HttpServletResponse> response) {
+        User user = request
+                .map(req -> req.getHeader("Authorization"))
+                .filter(id -> !id.isEmpty())
+                .map(id -> id.replace("Bearer ", ""))
+                .map(userRepository::findById)
+                .orElse(null);
+        return new AuthContext(user, request, response);
+    }
+
+    private static GraphQLSchema buildSchema() {
+        return SchemaParser.newParser()
+                .file("schema.graphqls")
+                .resolvers(
+                        new Query(linkRepository),
+                        new Mutation(linkRepository, userRepository),
+                        new SigninResolver())
+                .build()
+                .makeExecutableSchema();
+    }
+}
